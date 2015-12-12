6a22f4972a85435297493884289bac77b1b1f47a
Paul Bosselaar <paul@bosselaar.net>
2015-12-12 21:09:18 +0100

    Added day 12

---
+++ src/com/adventofcode/creepy/Day12p1.java
@@ -0,0 +1,61 @@
+package com.adventofcode.creepy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Day12p1 {
+	public static void main(String[] args) throws IOException {
+
+		StringBuilder json = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(Day12p1.class.getResourceAsStream("/input-day12.txt")));) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				json.append(line);
+			}
+		}
+		
+		JSONArray array = new JSONArray(json.toString());
+		
+		
+		System.out.println(parseArray(array));
+	}
+	
+	public static int parseIt(Object value) {
+		int result = 0;
+		if (value instanceof JSONArray) {
+			result += parseArray((JSONArray) value);
+		} else if (value instanceof JSONObject) {
+			result += parseObject((JSONObject) value);
+		} else if (value instanceof Integer ) { 
+			result += (Integer) value;
+		}
+		
+		
+		return result;
+	}
+	
+	public static int parseObject(JSONObject object) {
+		int result = 0;
+		for (String key: object.keySet()) {
+			Object value = object.get(key);
+			result += parseIt(value);
+			
+		}
+		return result;
+	}
+	
+	public static int parseArray(JSONArray array) {
+		int result = 0;
+		for (int i = 0; i < array.length(); i++) {
+			Object value = array.get(i);
+			result += parseIt(value);
+			
+		}
+		return result;
+	}
+}
+		
